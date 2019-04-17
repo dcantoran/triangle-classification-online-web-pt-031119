6ddb2f86d72da545da757ff79476fa7985a6a871
David Cantoran <davidcantoran36@gmail.com>
2019-04-17 19:47:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,13 @@ class Triangle
   def kind 
     
     
-    if @left == @right && @left == @bottom
-      :equilateral 
-    elsif @left == @right || @right == @bottom || @left == @bottom
-      :isosceles 
-    elsif @left != @right && @right != @bottom
-      :scalene
-    end 
+      if @left == @right && @left == @bottom
+        :equilateral 
+      elsif @left == @right || @right == @bottom ||   @left == @bottom
+        :isosceles 
+      elsif @left != @right && @right != @bottom
+        :scalene
+      end 
     
     if @right <= 0 || @left <= 0 || @bottom <= 0 
       raise TriangleError
